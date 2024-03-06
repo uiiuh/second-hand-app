3629e6895fbf56cd252bccc7da894edcab655942
sang <1446820882@qq.com>
2024-03-06 23:31:13 +0800

    登录页面ui

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import router from '@/router'
 Vue.use(VueRouter)
 
 // 按需引入 饿了么ui 组件
-import { Menu, MenuItem, Carousel, CarouselItem, Input, Button } from 'element-ui'
+import { Menu,MenuItem,Carousel,CarouselItem,Input,Button,Form,FormItem,} from 'element-ui'
 // 全局注册饿了么组件
 Vue.component(Menu.name, Menu)
 Vue.component(MenuItem.name, MenuItem)
@@ -17,6 +17,8 @@ Vue.component(Carousel.name, Carousel)
 Vue.component(CarouselItem.name, CarouselItem)
 Vue.component(Input.name, Input)
 Vue.component(Button.name, Button)
+Vue.component(Form.name, Form)
+Vue.component(FormItem.name, FormItem)
 
 // 关闭生产提示
 Vue.config.productionTip = false
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,15 +1,45 @@
 <template>
     <div class="login">
-        登录页
+        <div class="login-form">
+            <el-form :model="form" label-position="right" label-width="auto">
+                <el-form-item label="用户名">
+                    <el-input v-model="form.userName" placeholder="请输入 用户名/手机号/邮箱"></el-input>
+                </el-form-item>
+                <el-form-item label="密码">
+                    <el-input type="password" v-model="form.password" placeholder="请输入密码" show-password></el-input>
+                </el-form-item>
+            </el-form>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'Login'
+        name: 'Login',
+        data() {
+            return {
+                // 表单数据对象
+                form: {
+                    userName: '',
+                    password: ''
+                }
+            }
+        },
     }
 </script>
 
 <style scoped lang="less">
+    .login {
+        display: flex;
+        justify-content: flex-end;
+        padding: 15px 0;
+        .login-form {
+            margin-right: 190px;
+            padding: 15px;
+            width: 400px;
+            // height: 400px;
+            background-color: #E7E8FB;
 
+        }
+    }
 </style>
